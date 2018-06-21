bb1fecf1920c382648a9d0098b3e6188c50d2282
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2018-06-21 12:05:24 +0200

    [RFC] use templated isl types constructTensorTuple
    Templated isl types require the user to specify the domain and range
    universes of isl objects, allowing the compiler to check whether
    it makes sense to combine pairs of objects.
    This RFC only converts isPromotableToRegistersBelow and some related
    functions to illustrate the effect.
    The isPromotableToRegistersBelow was already applying operations
    correctly, so the code itself did not require any changes.
    However, one variable was reused to store different types
    of intermediate result and this one had to be split up into
    several variables because they now have different types.

---
+++ tc/core/polyhedral/domain_types.h
@@ -0,0 +1,15 @@
+namespace tc {
+namespace polyhedral {
+
+struct Prefix;
+struct Reduction;
+struct ReductionSchedule;
+struct Scope;
+struct Statement;
+struct Tag;
+struct Tensor;
+struct Thread;
+struct Warp;
+
+} // namespace polyhedral
+} // namespace tc
